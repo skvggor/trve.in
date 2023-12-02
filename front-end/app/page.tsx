3f657104c55363e6b40dc19ac63fbe2840da3617
Marcos Garcia de Lima <marckfree@gmail.com>
2023-12-02 13:51:11 -0300

    Add mobile avatar, reset and clean up

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,31 +1,61 @@
 "use client"
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
+import Image from "next/image"
+// import { Moon, Sun } from "lucide-react"
+// import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 
 
 export default function Home() {
-  // const [theme, setTheme] = React.useState<"dark" | "light">("dark")
-  const [mounted, setMounted] = React.useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  // const { theme, setTheme } = useTheme()
 
-  React.useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(true), [])
 
-  const clickHandler = () => {
-    setTheme(theme === "dark" ? "light" : "dark")
-  }
+  // const clickHandler = () => {
+  //   setTheme(theme === "dark" ? "light" : "dark")
+  // }
 
-  return (
+  return (mounted && (
     <main className="p-5">
-      <Button variant="outline" onClick={clickHandler}>
+      <header>
+        <div className="avatar
+          h-24
+          relative
+          rounded-full
+          w-24"
+        >
+          <Image
+            src="/avatar.webp"
+            alt="avatar"
+            width={200}
+            height={200}
+            className="rounded-full
+              border-2
+              border-green-700
+              p-0.5"
+          />
+
+          <span className="icon-status
+            absolute
+            bg-green-500
+            border-2
+            bottom-2
+            h-4
+            right-2
+            rounded-full
+            w-4"
+          />
+        </div>
+      </header>
+      {/* <Button variant="outline" onClick={clickHandler}>
         {theme === "light" && <Sun />}
         {theme === "dark" && <Moon />}
 
         <span className="sr-only">Toggle theme: Dark, Light</span>
-      </Button>
-    </main>
+      </Button> */}
+    </main>)
   )
 }
