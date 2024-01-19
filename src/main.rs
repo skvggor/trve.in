f4bf4ef4d3cc111ef9616227e417716e370ba231
Marcos Garcia de Lima <marckfree@gmail.com>
2024-01-19 20:19:47 -0300

    Add funtion to get UTC current time

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
+use chrono::prelude::*;
+use dotenvy::dotenv;
 use std::env;
 use std::net::SocketAddr;
-use dotenvy::dotenv;
 
-use axum::{
-    routing::get,
-    Router,
-};
+use serde::Serialize;
+
+use axum::{routing::get, Json, Router};
+
+#[derive(Serialize)]
+struct Time {
+    utc_current_time: String,
+}
+
+async fn current_time() -> Json<Time> {
+    let utc_current_time: String = Utc::now().format("%H:%M:%S").to_string();
+    let time: Time = Time { utc_current_time };
+    Json(time)
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().expect("Failed to load .env file");
 
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }));
+    let app: Router = Router::new().route("/", get(current_time));
 
-    let host = env::var("HOST").unwrap();
-    let port = env::var("PORT").unwrap();
+    let host: String = env::var("HOST").unwrap();
+    let port: String = env::var("PORT").unwrap();
 
-    let address = format!("{host}:{port}");
+    let address: String = format!("{host}:{port}");
 
-    let default_address = "0.0.0.0:3000"
-        .parse::<SocketAddr>()
-        .unwrap();
+    let default_address: SocketAddr = "0.0.0.0:3000".parse::<SocketAddr>().unwrap();
 
-    let final_addr = if let Ok(curr) = address.parse::<SocketAddr>() {
+    let final_addr: SocketAddr = if let Ok(curr) = address.parse::<SocketAddr>() {
         curr
     } else {
         default_address
