d7feb9f3826d5454a1214ff89837192596b51322
Marcos Garcia de Lima <marckfree@gmail.com>
2024-01-20 12:15:15 -0300

    Add status logic

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,75 @@ use serde::Serialize;
 use axum::{routing::get, Json, Router};
 
 #[derive(Serialize)]
-struct Time {
-    utc_current_time: String,
+struct Status {
+    status: String,
 }
 
-async fn current_time() -> Json<Time> {
-    let utc_current_time: String = Utc::now().format("%H:%M:%S").to_string();
-    let time: Time = Time { utc_current_time };
-    Json(time)
+fn get_now() -> DateTime<Utc> {
+    let now: DateTime<Utc> = Utc::now();
+    now
+}
+
+fn get_utc_time() -> String {
+    let utc_time: String = get_now().format("%H:%M:%S").to_string();
+    utc_time
+}
+
+fn get_week_day() -> String {
+    let week_day: String = get_now().format("%A").to_string();
+    week_day
+}
+
+fn get_status() -> String {
+    /*
+        Status:
+        - work: 08:00:00 - 18:00:00
+        - lunch: 12:00:00 - 14:00:00
+        - sleep: 00:00:00 - 06:00:00
+        - weekend: Saturday, Sunday
+    */
+
+    let utc_time: String = get_utc_time();
+    let week_day: String = get_week_day();
+    let mut status: String = "none".to_string();
+
+    let work: bool = utc_time >= "08:00:00".to_string() && utc_time < "18:00:00".to_string();
+    let lunch: bool = utc_time >= "12:00:00".to_string() && utc_time < "14:00:00".to_string();
+    let sleep: bool = utc_time >= "00:00:00".to_string() && utc_time < "06:00:00".to_string();
+    let weekend: bool = week_day == "Saturday".to_string() || week_day == "Sunday".to_string();
+
+    if weekend {
+        status = "weekend".to_string();
+    } else if sleep {
+        status = "sleep".to_string();
+    } else if lunch {
+        status = "lunch".to_string();
+    } else if work {
+        status = "work".to_string();
+    }
+
+    status
+}
+
+async fn response_json() -> Json<Status> {
+    let current_status: String = get_status();
+
+    let status: Status = Status {
+        status: current_status,
+    };
+
+    Json(status)
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().expect("Failed to load .env file");
 
-    let app: Router = Router::new().route("/", get(current_time));
+    let route: String = "/rust-api/api/v1".to_string();
+
+    let app: Router = Router::new()
+        .route(&(route.clone() + "/status"), get(response_json))
+        .route(&(route + "/healthcheck"), get(|| async { "ok" }));
 
     let host: String = env::var("HOST").unwrap();
     let port: String = env::var("PORT").unwrap();
@@ -41,4 +95,6 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    println!("Server running on http://{}", final_addr);
 }
